b312ea46e4fc3e909d74034dfa51be3b91ec061b
honam <75104291+honambsn@users.noreply.github.com>
2023-11-09 10:53:20 +0700

    add buttons to main view and resize window

--- View/MainView.xaml.cs
+++ View/MainView.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using System.Windows.Interop;
+
 
 namespace WPF_LoginForm.View
 {
@@ -22,6 +26,26 @@ namespace WPF_LoginForm.View
         public MainView()
         {
             InitializeComponent();
+            
         }
-    }
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+            private void RadioButton_Checked(object sender, RoutedEventArgs e)
+            {
+
+            }
+
+            private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+            {
+                WindowInteropHelper helper = new WindowInteropHelper(this);
+                SendMessage(helper.Handle, 161, 2, 0);
+            }
+
+            private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+            {
+                this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            }
+        }
 }
